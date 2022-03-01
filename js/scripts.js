26a1d22513b8eccb0ffce3b8f5d5c5e885de08c3
cohencha <74265514+cohencha@users.noreply.github.com>
2022-02-28 22:56:36 -0500

    helppp
    ah

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,63 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiY29oZW5jaGEiLCJhIjoiY2t6dW12Zm1mMDhnbzJvbXl1cmYxY3BpNyJ9.qTcZ7QT6P96dIYJjSniWVw'
 
-// lngLat for the fountain in Washington Square Park
-var wspCenter = [-73.997456, 40.730831]
+$.getJSON('./data/ferry-stops-updates.geojson', function(nycferrystops) {
+  console.log(nycferrystops)
+
+var nycCenter = [-73.997456, 40.730831]
 
 var map = new mapboxgl.Map({
   container: 'mapContainer', // HTML container id
   style: 'mapbox://styles/mapbox/dark-v9', // style URL
-  center: wspCenter, // starting position as [lng, lat]
+  center: nycCenter, // starting position as [lng, lat]
   zoom: 9,
   // minZoom: 9,
   // maxZoom: 14
 });
 
-map.on('load', function() {
-    map.addSource('nycferrystops', {
-      type: 'geojson',
-      data: './data/ferry-stops-update.geojson'
-    })
+  map.addSource('nyc-ferry-pier', {
+    type: 'geojson',
+    data: './data/ferry-stops-updates.geojson'
+  })
 
-    nycferrystops.forEach(function(nycferrystops) {
+    nycferrystop.forEach(function(nycferrystops) {
       var mapMarker = new mapboxgl.Marker({
         color: '#2596be'
       })
-    .setLngLat([nycferrystops.geometry.coordinates])
-    .addTo(map);
-    })
-
-  map.addSource('nyc-mhi', {
-    type: 'geojson',
-    data: './data/nyc-mhi.geojson'
+      .setLngLat(nycferrystops.geometries.coordinates)
+      .addTo(map);
   })
 
-    map.addLayer({
-      'id': 'nyc-mhi-fill',
-      'type': 'fill',
-      'source': 'nyc-mhi',
-      'layout': {
-        'visibility': 'visible'
-      },
-      'paint': {
-        'fill-color': [
-          'interpolate',
-          ['linear'],
-          ['get', 'incomeedit_Refactored_estimate'],
-          0,
-          '#edf8fb',
-          33966,
-          '#b2e2e2',
-          55898,
-          '#66c2a4',
-          70390,
-          '#2ca25f',
-          93366,
-          '#006d2c',
-        ],
-        'fill-outline-color': '#ccc',
-        'fill-opacity': 0.75
-      }
-    });
+  // map.addSource('nyc-mhi', {
+  //   type: 'geojson',
+  //   data: './data/nyc-mhi.geojson'
+  // })
+  //
+  //   map.addLayer({
+  //     'id': 'nyc-mhi-fill',
+  //     'type': 'fill',
+  //     'source': 'nyc-mhi',
+  //     'layout': {
+  //       'visibility': 'visible'
+  //     },
+  //     'paint': {
+  //       'fill-color': [
+  //         'interpolate',
+  //         ['linear'],
+  //         ['get', 'incomeedit_Refactored_estimate'],
+  //         0,
+  //         '#edf8fb',
+  //         33966,
+  //         '#b2e2e2',
+  //         55898,
+  //         '#66c2a4',
+  //         70390,
+  //         '#2ca25f',
+  //         93366,
+  //         '#006d2c',
+  //       ],
+  //       'fill-outline-color': '#ccc',
+  //       'fill-opacity': 0.75
+  //     }
+  //   });
 
 })
