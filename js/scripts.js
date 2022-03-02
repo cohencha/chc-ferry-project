3494e4ad26ab95a8bd1737e2b61679b0e29e9b4a
cohencha <74265514+cohencha@users.noreply.github.com>
2022-03-01 19:23:48 -0500

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,7 @@ var map = new mapboxgl.Map({
   style: 'mapbox://styles/mapbox/dark-v9', // style URL
   center: nycCenter, // starting position as [lng, lat]
   zoom: 9,
-  // minZoom: 9,
-  // maxZoom: 14
+  maxZoom: 10
 });
 
   nycferrystops.features.forEach(function(nycferrystop) {
@@ -21,38 +20,5 @@ var map = new mapboxgl.Map({
     .setLngLat(nycferrystop.geometry.coordinates)
     .addTo(map);
   })
-
-  // map.addSource('nyc-mhi', {
-  //   type: 'geojson',
-  //   data: './data/nyc-mhi.geojson'
-  // })
-  //
-  //   map.addLayer({
-  //     'id': 'nyc-mhi-fill',
-  //     'type': 'fill',
-  //     'source': 'nyc-mhi',
-  //     'layout': {
-  //       'visibility': 'visible'
-  //     },
-  //     'paint': {
-  //       'fill-color': [
-  //         'interpolate',
-  //         ['linear'],
-  //         ['get', 'incomeedit_Refactored_estimate'],
-  //         0,
-  //         '#edf8fb',
-  //         33966,
-  //         '#b2e2e2',
-  //         55898,
-  //         '#66c2a4',
-  //         70390,
-  //         '#2ca25f',
-  //         93366,
-  //         '#006d2c',
-  //       ],
-  //       'fill-outline-color': '#ccc',
-  //       'fill-opacity': 0.75
-  //     }
-  //   });
 
 })
